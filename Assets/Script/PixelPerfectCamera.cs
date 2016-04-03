97221a76481780cc34169b452b663dc34a2450e2
Minh Hoang <minhnh.da@gmail.com>
2016-04-03 11:16:32 +0700

    implement adaptation for all screen

---
+++ Assets/Script/PixelPerfectCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PixelPerfectCamera : MonoBehaviour {
+
+//	public float pixelToUnits = 1f;
+//	public float scale = 1f;
+//
+//    public Vector2 nativeResolution = new Vector2(1136, 640);
+
+	public float targetRatio;
+
+	// Use this for initialization
+	void Awake () {
+		Camera cam = GetComponent<Camera>();
+		cam.aspect = targetRatio;
+	}
+}
