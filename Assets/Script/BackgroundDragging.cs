f97faac3e9b4c611fc5cd264220bb3b2c2837abc
minhnh <minhnh.da@gmail.com>
2016-04-09 17:38:40 +0700

    dmm unity bố quỳ với

--- Assets/Script/BackgroundDragging.cs
+++ Assets/Script/BackgroundDragging.cs
@@ -3,25 +3,31 @@ using System.Collections;
 
 public class BackgroundDragging : MonoBehaviour {
 
-	Vector3 screenPoint;
 	Vector3 offset;
 	float firstMousePosX;
 
 	void OnMouseDown() {
-		offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+		offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
 		firstMousePosX = Input.mousePosition.x;
+
+
 	}
 
 	void OnMouseDrag()
 	{
-		Vector3 curScreenPoint = new Vector3(firstMousePosX, Input.mousePosition.y, screenPoint.z);
+		Vector3 curScreenPoint = new Vector3(firstMousePosX, Input.mousePosition.y, 0);
 		Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
 		if (curPosition.y < 0)
 		{
 			// Bg is outta screen
 			curPosition.y = 0;
 		}
+		else if (curPosition.y > 7.785632f)
+		{
+			curPosition.y = 7.785632f;
+		}
 		transform.position = curPosition;
 	}
+
 
 }
