abd794c6d3cec3c33bcce4fbb46650af6b983553
Hoang Viet <cooro1994@gmail.com>
2016-04-12 23:46:36 +0700

    fancy menu

---
+++ Assets/Script/InGameGUI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InGameGUI : MonoBehaviour {
+	private bool menuPanelOpen = false;
+	public TweenPosition menuPanelTweener;
+	public TweenRotation menuPanelArrowTweener;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void ToggleMenuPanel(){
+		if (menuPanelOpen) {
+			menuPanelTweener.Play (false);
+			menuPanelArrowTweener.Play (false);
+			menuPanelOpen = false;
+		} else {
+			menuPanelTweener.Play (true);
+			menuPanelArrowTweener.Play (true);
+			menuPanelOpen = true;
+		}
+	}
+}
