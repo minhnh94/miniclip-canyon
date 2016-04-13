c5aa4596d30bab8f22f7776c4e5245f250c1981b
Hiếu Trần Trung <kagoromo@gmail.com>
2016-04-13 20:10:32 +0700

    PolyNav with Enemy sample

---
+++ Assets/Script/MoveToGoal.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+//example
+[RequireComponent(typeof(PolyNavAgent))]
+public class MoveToGoal : MonoBehaviour{
+
+	private PolyNavAgent _agent;
+	public PolyNavAgent agent{
+		get
+		{
+			if (!_agent)
+				_agent = GetComponent<PolyNavAgent>();
+			return _agent;			
+		}
+	}
+
+	void Update() {
+		agent.SetDestination(GameObject.Find("Goal").transform.position);
+	}
+}
