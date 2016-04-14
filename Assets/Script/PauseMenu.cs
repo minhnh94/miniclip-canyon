042a79e0905053fcd88983191b38f7d25931e12b
Hoang Viet <cooro1994@gmail.com>
2016-04-15 00:37:53 +0700

    paused

---
+++ Assets/Script/PauseMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+	public static bool isPaused;
+	public GameObject pauseMenuCanvas;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isPaused) {
+			pauseMenuCanvas.SetActive (true);
+			Time.timeScale = 0f;
+		} else {
+			pauseMenuCanvas.SetActive (false);
+			Time.timeScale = 1f;
+		}
+	}
+
+	public void Resume(){
+		isPaused = false;
+	}
+
+	public void Quit(){
+		Application.LoadLevel ("Menu");
+	}
+}
