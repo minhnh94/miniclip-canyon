7ee7ec4723be50410f2639b079de676ec4f35fa3
Hiếu Trần Trung <kagoromo@gmail.com>
2016-04-15 12:09:37 +0700

    Keep Moving Bullets: The Prelude

--- Assets/Script/BulletBehavior.cs
+++ Assets/Script/BulletBehavior.cs
@@ -6,7 +6,7 @@ public class BulletBehavior : MonoBehaviour {
 	public int damage;
 	public GameObject target;
 	public Vector3 startPosition;
-	public Vector3 targetPosition;
+	private Vector3 oldTargetPosition;
 	private EnemyDestructionDelegate del;
 	private bool enemyDestruction;
 	private bool enemyCollision;
@@ -19,7 +19,7 @@ public class BulletBehavior : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		startTime = Time.time;
-		distance = Vector3.Distance(startPosition, targetPosition);
+		distance = Vector3.Distance(startPosition, target.transform.position);
 		GameObject gm = GameObject.Find ("GameManager");
 		gameManager = gm.GetComponent<GameManagerBehavior> ();
 		del = target.gameObject.GetComponent<EnemyDestructionDelegate>();
@@ -35,31 +35,39 @@ public class BulletBehavior : MonoBehaviour {
 		// The bullet will keep moving, staying below the enemy until the enemy is destroyed
 		// Dunno how to fix tbh
 		if (target != null && !enemyDestruction) {
-			gameObject.transform.position = Vector3.Lerp(startPosition, target.transform.position, timeInterval * speed / distance);
+			gameObject.transform.position = Vector3.Lerp (startPosition, target.transform.position, timeInterval * speed / distance);
+		} else {
+			gameObject.transform.position = Vector3.MoveTowards (gameObject.transform.position, oldTargetPosition, - (timeInterval * speed / (distance * 25)) );
 		}
 	}
 
 	void OnTriggerEnter2D (Collider2D other) {
 		// If bullet hit the enemy
-		if (other == target.transform.GetComponent<PolygonCollider2D> ()) {
+		if ((target != null) && (other == target.transform.GetComponent<PolygonCollider2D> ())) {
 			Transform healthBarTransform = target.transform.Find ("HealthBarWrapper/HealthBar");
 			HealthBar healthBar = healthBarTransform.gameObject.GetComponent<HealthBar> ();
 			healthBar.currentHealth -= Mathf.Max (damage, 0);
 			Debug.Log (healthBar.currentHealth);
 
 			if (healthBar.currentHealth <= 0) {
+				oldTargetPosition = target.transform.position;
 				Destroy (target);
 				Destroy (gameObject);
 				enemyCollision = true;
 				gameManager.Gold += 20;
 			}
-		} 
+		}
+
+		if (other.tag == "World Boundary") {
+			Destroy (gameObject);
+		}
 	}
 
 	void OnEnemyDestroy(GameObject enemy) {
 		del.enemyDelegate -= OnEnemyDestroy;
 		enemyDestruction = true;
-		enemyCollision = true;
-		Destroy (gameObject);
+		if (enemyCollision) {
+			Destroy (gameObject);
+		}
 	}
 }
