a1973b77adc545a87c24370587ea791bc9bf6a29
minhnh <minhnh.da@gmail.com>
2016-04-19 22:52:25 +0700

    highlight button done

--- Assets/Script/MapSelectButtonAction.cs
+++ Assets/Script/MapSelectButtonAction.cs
@@ -7,18 +7,35 @@ public class MapSelectButtonAction : MonoBehaviour {
 	public GameObject mapPreview;
 
 	void Start() {
+		if (gameObject.tag.Equals(Constant.SelectMap1Btn))
+		{
+			GetComponent<Image>().sprite = Resources.Load<Sprite>("map/map1_selected");
+		}
+
 		GetComponent<Button>().onClick.AddListener(() =>
 			{
 				var btnTag = gameObject.tag;
 				if (btnTag.Equals(Constant.SelectMap1Btn)) {
 					mapPreview.GetComponent<SelectGameMapPreview>().SetMapPreview(0);
 					GameObject.Find("LevelText").GetComponent<Text>().text = "Desert";
+
+					GetComponent<Image>().sprite = Resources.Load<Sprite>("map/map1_selected");
+					GameObject.Find("map2_button").GetComponent<Image>().sprite = Resources.Load<Sprite>("map/map2");
+					GameObject.Find("map3_button").GetComponent<Image>().sprite = Resources.Load<Sprite>("map/map3");
 				} else if (btnTag.Equals(Constant.SelectMap2Btn)) {
 					mapPreview.GetComponent<SelectGameMapPreview>().SetMapPreview(1);
 					GameObject.Find("LevelText").GetComponent<Text>().text = "Wasteland";
+
+					GetComponent<Image>().sprite = Resources.Load<Sprite>("map/map2_selected");
+					GameObject.Find("map1_button").GetComponent<Image>().sprite = Resources.Load<Sprite>("map/map1");
+					GameObject.Find("map3_button").GetComponent<Image>().sprite = Resources.Load<Sprite>("map/map3");
 				} else if (btnTag.Equals(Constant.SelectMap3Btn)) {
 					mapPreview.GetComponent<SelectGameMapPreview>().SetMapPreview(2);
 					GameObject.Find("LevelText").GetComponent<Text>().text = "Canyon";
+
+					GetComponent<Image>().sprite = Resources.Load<Sprite>("map/map3_selected");
+					GameObject.Find("map2_button").GetComponent<Image>().sprite = Resources.Load<Sprite>("map/map2");
+					GameObject.Find("map1_button").GetComponent<Image>().sprite = Resources.Load<Sprite>("map/map1");
 				}
 			});
 	}
--- Assets/Script/SelectGameModeButtonAction.cs
+++ Assets/Script/SelectGameModeButtonAction.cs
@@ -4,16 +4,33 @@ using UnityEngine.UI;
 
 public class SelectGameModeButtonAction : MonoBehaviour {
 	void Start() {
+		if (gameObject.tag.Equals(Constant.EasyModeBtn))
+		{
+			GetComponent<Image>().sprite = Resources.Load<Sprite>("icon/border_button_select");
+		}
+
 		GetComponent<Button>().onClick.AddListener(() =>
 			{
 				var difficultyText = GameObject.Find("DifficultyText").GetComponent<Text>();
 				var btnTag = gameObject.tag;
 				if (btnTag.Equals(Constant.EasyModeBtn)) {
 					difficultyText.text = "Waves: 20\nDelay per wave: 2 min\nScore bonus: 0%";
+
+					GetComponent<Image>().sprite = Resources.Load<Sprite>("icon/border_button_select");
+					GameObject.Find("Button (2)").GetComponent<Image>().sprite = Resources.Load<Sprite>("icon/border_button");
+					GameObject.Find("Button (1)").GetComponent<Image>().sprite = Resources.Load<Sprite>("icon/border_button");
 				} else if (btnTag.Equals(Constant.NormalModeBtn)) {
 					difficultyText.text = "Waves: 30\nDelay per wave: 1.5 min\nScore bonus: 20%";
+
+					GetComponent<Image>().sprite = Resources.Load<Sprite>("icon/border_button_select");
+					GameObject.Find("Button (2)").GetComponent<Image>().sprite = Resources.Load<Sprite>("icon/border_button");
+					GameObject.Find("Button").GetComponent<Image>().sprite = Resources.Load<Sprite>("icon/border_button");
 				} else if (btnTag.Equals(Constant.HardModeBtn)) {
 					difficultyText.text = "Waves: 50\nDelay per wave: 1 min\nScore bonus: 50%";
+
+					GetComponent<Image>().sprite = Resources.Load<Sprite>("icon/border_button_select");
+					GameObject.Find("Button").GetComponent<Image>().sprite = Resources.Load<Sprite>("icon/border_button");
+					GameObject.Find("Button (1)").GetComponent<Image>().sprite = Resources.Load<Sprite>("icon/border_button");
 				}
 			});
 	}
