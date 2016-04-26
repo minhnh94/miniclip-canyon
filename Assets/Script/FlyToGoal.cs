5ed0d4916a08127599ce29750c3c3dfa3c81c278
Hiếu Trần Trung <kagoromo@gmail.com>
2016-04-26 22:25:23 +0700

    Slow effect on air enemies

--- Assets/Script/FlyToGoal.cs
+++ Assets/Script/FlyToGoal.cs
@@ -17,7 +17,7 @@ public class FlyToGoal : MonoBehaviour{
 
 	void Start() {
 		lastWaypointSwitchTime = Time.time;
-		RotateIntoMoveDirection ();
+		transform.position = waypoints [currentWaypoint].transform.position;
 	}
 
 	void Update() {
@@ -25,41 +25,38 @@ public class FlyToGoal : MonoBehaviour{
 		Vector3 endPosition = waypoints [currentWaypoint + 1].transform.position;
 
 		float pathLength = Vector3.Distance (startPosition, endPosition);
-		float totalTimeForPath = pathLength / speed;
-		float currentTimeOnPath = Time.time - lastWaypointSwitchTime;
-		gameObject.transform.position = Vector3.Lerp (startPosition, endPosition, currentTimeOnPath / totalTimeForPath );
+		Vector3 direction = endPosition - startPosition;
+		transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (Vector3.forward, direction), 1f);
+		transform.Translate (Vector3.up * speed * Time.deltaTime);
 
-		if (gameObject.transform.position.Equals(endPosition)) {
-			if (currentWaypoint < waypoints.Length - 3) {
-				currentWaypoint++;
-				lastWaypointSwitchTime = Time.time;
-			} else {
-				Destroy(gameObject);
-
-//				AudioSource audioSource = gameObject.GetComponent<AudioSource>();
-//				AudioSource.PlayClipAtPoint(audioSource.clip, transform.position);
-				GameManagerBehavior gameManager = GameObject.Find ("GameManager").GetComponent<GameManagerBehavior> ();
-				gameManager.Health -= 1;
-			}
+		if (isSlowed && ((Time.time - slowBegin) >= (baseSlowDuration + bulletSlowDuration))) {
+				ClearSlowEffect ();
 		}
-		if (isSlowed && (Time.time - slowBegin) >= (baseSlowDuration + bulletSlowDuration)) {
-			ClearSlowEffect();
-		}
-	}
-
-	private void RotateIntoMoveDirection() {
-		gameObject.GetComponent<SpriteRenderer> ().flipY = true;
 	}
 
 	void OnTriggerEnter2D (Collider2D other) {
 		if (other.tag == "Bullet") {
 			if (other.gameObject.GetComponent<BulletBehavior> ().isSlowBullet) {
 				if (!isSlowed) {
-//					speed /= 2;
+					speed /= 2;
 				}
 				isSlowed = true;
 				bulletSlowDuration = other.gameObject.GetComponent <BulletBehavior> ().bulletSlowDuration;
 				slowBegin = Time.time;
+			}
+		}
+
+		if (other.tag == "Finish") {
+			if (currentWaypoint < waypoints.Length - 3) {
+				currentWaypoint++;
+				lastWaypointSwitchTime = Time.time;
+			} else {
+				Destroy(gameObject);
+
+				//				AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+				//				AudioSource.PlayClipAtPoint(audioSource.clip, transform.position);
+				GameManagerBehavior gameManager = GameObject.Find ("GameManager").GetComponent<GameManagerBehavior> ();
+				gameManager.Health -= 1;
 			}
 		}
 	}
@@ -75,6 +72,6 @@ public class FlyToGoal : MonoBehaviour{
 	void ClearSlowEffect()
 	{
 		isSlowed = false;
-//		speed *= 2;
+		speed *= 2;
 	}
 }
