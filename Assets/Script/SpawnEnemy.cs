4449881cf2b31ae237c019f70274ea85059a36c4
minhnh <minhnh.da@gmail.com>
2016-04-29 20:41:10 +0700

    high score

--- Assets/Script/SpawnEnemy.cs
+++ Assets/Script/SpawnEnemy.cs
@@ -82,7 +82,7 @@ public class SpawnEnemy : MonoBehaviour {
 			gameManager.gameOver = true;
 //			GameObject gameOverText = GameObject.FindGameObjectWithTag ("GameWon");
 //			gameOverText.GetComponent<Animator>().SetBool("gameOver", true);
-			SceneManager.LoadScene("GameWonScene");
+			SceneManager.LoadScene("GameOverScene");
 		}
 	}
 }
