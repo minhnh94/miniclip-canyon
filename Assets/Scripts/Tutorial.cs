557fdc507e177f6f18337d43e738db79ec554fba
Hiếu Trần Trung <kagoromo@gmail.com>
2016-05-05 01:04:15 +0700

    Show tutorial button

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tutorial : MonoBehaviour {
+
+	public GameObject tutorialScreen;
+
+	public void OnClick() {
+		tutorialScreen.GetComponent<Tutorial> ().DisplayTutorial ();
+	}
+
+	public void DisplayTutorial() {
+		gameObject.GetComponent<Animator> ().SetTrigger ("displayTutorial");
+	}
+}
