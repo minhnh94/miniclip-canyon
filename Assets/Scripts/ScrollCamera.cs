3b4d6fa41d0219cc65c6f07045604f613769dca9
Hiếu Trần Trung <kagoromo@gmail.com>
2016-05-07 13:46:44 +0700

    Add TutorialGeneral

--- Assets/Scripts/ScrollCamera.cs
+++ Assets/Scripts/ScrollCamera.cs
@@ -4,9 +4,14 @@ using System.Collections;
 public class ScrollCamera : MonoBehaviour {
 
 	public GameObject tutorialGoal;
+	public GameObject tutorialGeneral;
 
 	public void DisplayTutorialGoal() {
 		tutorialGoal.GetComponent<TutorialGoal> ().DisplayTutorial ();
+	}
+
+	public void DisplayTutorialGeneral() {
+		tutorialGeneral.GetComponent<TutorialGeneral> ().DisplayTutorial ();
 	}
 
 	public void RemoveAnimator() {
