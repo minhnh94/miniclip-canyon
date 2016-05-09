1771ac5fa7baf9976f6b92d62ae40f87f08f991d
minhnh <minhnh.da@gmail.com>
2016-05-09 22:31:27 +0700

    building option scene

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -1,10 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour {
 
 	public void StartSelectMap() {
 		print ("clicked");
 		Application.LoadLevel("SelectMap");
+	}
+
+	public void GoToOption() {
+		SceneManager.LoadScene("OptionScene");
 	}
 }
